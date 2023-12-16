3c184fabc6ec9e578bbc29f797a07c9dd713498b
Bitmun <pashabalko21@gmail.com>
2023-12-16 18:02:34 +0300

    refactor: refactor sidepanel logic

--- index.js
+++ index.js
@@ -114,19 +114,66 @@ const appendHeaderEl = (el) => {
 };
 
 document.addEventListener('DOMContentLoaded', () => {
-  let isOpened = false;
-  document.getElementById('burger-icon').addEventListener('click', (e) => {
-    if (!isOpened) {
-      openNav();
+  window.addEventListener('resize', handleResize);
+
+  let lastClickedTop = null;
+  let lastClickedBottom = null;
+  let sidePanel = document.getElementById('side-panel');
+  let closeBtn = document.getElementById('closebtn');
+  const icon = document.getElementById('burger-icon');
+
+  closeBtn.addEventListener('click', () => {
+    if (lastClickedTop !== null) {
+      lastClickedTop.classList.remove('sidepanel-top-is-clicked');
+      lastClickedBottom.classList.remove('is-opened');
     }
+    sidePanel.classList.remove('sidepanel-opened');
   });
 
-  headerItems.map((item) => appendHeaderEl(item));
+  icon.addEventListener('click', () => {
+    sidePanel.classList.toggle('sidepanel-opened');
+  });
+
+  headerItems.map((item) => {
+    appendHeaderEl(item);
+    appendSidePanelEl(item);
+  });
+
   searchItemsList.map((item) => appendBlock(item));
+  const topSideButtons = document.querySelectorAll('.sidepanel-top');
+
+  topSideButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+      if (lastClickedTop !== null && lastClickedTop !== button) {
+        lastClickedBottom.classList.remove('is-opened');
+        lastClickedTop.classList.remove('sidepanel-top-is-clicked');
+      }
+      if (lastClickedTop !== null) {
+        lastClickedTop.classList.remove('sidepanel-top-is-clicked');
+      }
+      button.classList.toggle('sidepanel-top-is-clicked');
+      const ul = button.nextElementSibling;
+      ul.classList.toggle('is-opened');
+      lastClickedTop = button;
+      lastClickedBottom = ul;
+    });
+  });
 });
 
 const appendSidePanelEl = (item) => {
   const sidePanel = document.getElementById('side-panel');
+  const mainStr = document.createElement('li');
+  mainStr.classList.add('sidepanel-top');
+  mainStr.innerHTML = `
+  <a>${item.text}</a>
+  `;
+  const innerUl = document.createElement('ul');
+  item.submenu.map((el) => {
+    innerUl.innerHTML += `<li>${el}</li>`;
+  });
+  innerUl.classList.add('is-closed');
+  sidePanel.appendChild(mainStr);
+  sidePanel.appendChild(innerUl);
 };
 
 function handleResize() {
@@ -136,19 +183,9 @@ function handleResize() {
   }
 }
 
-function openNav() {
-  let sidePanel = document.getElementById('side-panel');
-
-  sidePanel.style.height = '100%';
-  sidePanel.style.paddingTop = '60px';
-  sidePanel.style.paddingLeft = '30px';
-  sidePanel.style.paddingRight = '30px';
-}
-
 function closeNav() {
   let sidePanel = document.getElementById('side-panel');
-  sidePanel.style.height = '0';
-  sidePanel.style.paddingTop = '0';
+  sidePanel.classList.remove('sidepanel-opened');
 }
 
 function inputFunction() {
