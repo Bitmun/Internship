3540ac536e8dc0471c54dd44474709649de187dd
Bitmun <pashabalko21@gmail.com>
2023-12-16 16:41:02 +0300

    feat: add search bar

--- index.js
+++ index.js
@@ -33,71 +33,101 @@ let headerItems = [
   { text: 'Community', submenu: ['Submenu 1', 'Submenu 2', 'Submenu 3'] },
 ];
 
-handleResize();
+let searchItemsList = [
+  {
+    title: 'Spring Boot',
+    desc: 'Takes an opinionated view of building Spring applications and gets you up and running as quickly as possible.',
+    img: 'https://spring.io/img/projects/spring-boot.svg',
+  },
+  {
+    title: 'Spring Framework',
+    desc: 'Provides core support for dependency injection, transaction management, web apps, data access, messaging, and more.',
+    img: 'https://spring.io/img/projects/spring-framework.svg?v=2',
+  },
+  {
+    title: 'Spring Data',
+    desc: 'Provides a consistent approach to data access – relational, non-relational, map-reduce, and beyond.',
+    img: 'https://spring.io/img/projects/spring-data.svg',
+  },
+  {
+    title: 'Spring Cloud Data Flow',
+    desc: 'Provides an orchestration service for composable data microservice applications on modern runtimes.',
+    img: 'https://spring.io/img/projects/spring-data-flow.svg',
+  },
+  {
+    title: 'Spring Cloud',
+    desc: 'Provides a set of tools for common patterns in distributed systems. Useful for building and deploying microservices.',
+    img: 'https://spring.io/img/projects/spring-cloud.svg',
+  },
+  {
+    title: 'Spring Security',
+    desc: 'Provides an orchestration service for composable data microservice applications on modern runtimes.',
+    img: 'https://spring.io/img/projects/spring-security.svg',
+  },
+];
 
-window.addEventListener('resize', handleResize);
+let delayTimer;
 
-document.addEventListener('DOMContentLoaded', function () {
-  let sidePanel = document.getElementById('mySidepanel');
-  headerItems.forEach((item) => {
-    let liElement = document.createElement('li');
-    let aElement = document.createElement('a');
-    liElement.classList.toggle('top-burger-wrapper');
-    aElement.textContent = item.text;
-    aElement.classList.toggle('aElement');
+const appendBlock = (blockData) => {
+  const blocksSection = document.getElementById('blocks-section');
+  const { title, desc, img } = blockData;
+  const container = document.createElement('div');
 
-    let innerUl = document.createElement('ul');
-    innerUl.classList.toggle('innerList');
-    innerUl.style.display = 'none';
-    item.submenu.forEach((subItem) => {
-      let subLiElement = document.createElement('li');
-      subLiElement.classList.toggle('inner-burger-wrapper');
-      let subAElement = document.createElement('a');
-      subAElement.href = '#';
-      subAElement.textContent = subItem;
-      subLiElement.appendChild(subAElement);
-      innerUl.appendChild(subLiElement);
-    });
+  container.classList.add('block-container');
+  container.innerHTML = `
+  <article class="block-wrapper">
+  <a class="block">
+    <div class="image-container">
+      <img src="${img}" />
+    </div>
+    <div class="block-text-container">
+      <h1>${title}</h1>
+      <p>
+        ${desc}
+      </p>
+    </div>
+  </a>
+</article>
+  `;
+  blocksSection.appendChild(container);
+};
 
-    liElement.appendChild(aElement);
-    liElement.appendChild(innerUl);
-    sidePanel.appendChild(liElement);
+const appendHeaderEl = (el) => {
+  const { text, submenu } = el;
+  let ul = document.getElementById('navigation-ul');
+  const topLi = document.createElement('li');
+  topLi.classList.add('dropdown');
+  topLi.innerHTML = `
+  <a href="#">${text}</a>
+  `;
+  const innerUl = document.createElement('ul');
+  innerUl.classList.add('dropdown-menu');
+  submenu.map((el) => {
+    innerUl.innerHTML += `
+      <li>
+        <p>${el}</p>  
+      </li>
+    `;
+  });
+  topLi.appendChild(innerUl);
+  ul.appendChild(topLi);
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  let isOpened = false;
+  document.getElementById('burger-icon').addEventListener('click', (e) => {
+    if (!isOpened) {
+      openNav();
+    }
   });
 
-  var outerLiElements = document.querySelectorAll('#mySidepanel li');
-  var openedUL = null;
-
-  outerLiElements.forEach((outerLi) => {
-    outerLi.addEventListener('click', function () {
-      var innerUl = this.querySelector('.innerList');
-      var anchorTags = this.parentNode.querySelectorAll('a');
-
-      // Close previously opened list
-      if (openedUL && openedUL !== innerUl) {
-        openedUL.style.display = 'none';
-      }
-      // Close previously opened list
-      if (openedUL && openedUL !== innerUl) {
-        openedUL.style.display = 'none';
-        var previousAnchorTag = openedUL.parentNode.querySelector('a');
-        previousAnchorTag.style.color = 'white';
-      }
-      console.log(innerUl);
-      // Toggle visibility of clicked list
-      innerUl.style.display =
-        innerUl.style.display === 'none' ? 'block' : 'none';
-
-      // Toggle color of anchor tags
-      anchorTags.forEach((anchorTag) => {
-        anchorTag.style.color = 'white';
-      });
-
-      this.querySelector('a').style.color = 'green';
-
-      openedUL = innerUl;
-    });
-  });
+  headerItems.map((item) => appendHeaderEl(item));
+  searchItemsList.map((item) => appendBlock(item));
 });
+
+const appendSidePanelEl = (item) => {
+  const sidePanel = document.getElementById('side-panel');
+};
 
 function handleResize() {
   var windowWidth = window.innerWidth;
@@ -107,7 +137,8 @@ function handleResize() {
 }
 
 function openNav() {
-  let sidePanel = document.getElementById('mySidepanel');
+  let sidePanel = document.getElementById('side-panel');
+
   sidePanel.style.height = '100%';
   sidePanel.style.paddingTop = '60px';
   sidePanel.style.paddingLeft = '30px';
@@ -115,7 +146,38 @@ function openNav() {
 }
 
 function closeNav() {
-  let sidePanel = document.getElementById('mySidepanel');
+  let sidePanel = document.getElementById('side-panel');
   sidePanel.style.height = '0';
   sidePanel.style.paddingTop = '0';
+}
+
+function inputFunction() {
+  const inputElement = document.getElementById('search-input');
+  const query = inputElement.value.trim().toLowerCase();
+  delay(() => {
+    searchItems(query);
+  }, 300);
+}
+
+function searchItems(query) {
+  const filteredItems = searchItemsList.filter(
+    (item) =>
+      item.title.toLowerCase().includes(query) ||
+      item.desc.toLowerCase().includes(query)
+  );
+  let parent = document.getElementById('blocks-section');
+  console.log(filteredItems);
+  if (filteredItems.length === 0) {
+    parent.innerHTML = 'No results';
+    parent.classList.add('is-empty');
+  } else {
+    parent.classList.remove('is-empty');
+    parent.innerHTML = '';
+    filteredItems.map((item) => appendBlock(item));
+  }
+}
+
+function delay(func, delay) {
+  clearTimeout(delayTimer);
+  delayTimer = setTimeout(func, delay);
 }
